5afcad3543b20f7a5c0bbdca16d2885b1b73ee67
Kusumkoli <kusumkoli372@gmail.com>
2020-03-26 00:00:16 -1200

    login page

--- LoginPageTask/main.js
+++ LoginPageTask/main.js
@@ -1,1 +1,17 @@
 /* ========================WRITE YOUR JS FROM HERE======================== */
+(function($){
+  var search_button = $('.open_form'),
+      close_button  = $('.close'),
+      input = $('.input');
+  search_button.on('click',function(){
+    $(this).parent().addClass('open');
+    close_button.fadeIn(500);
+    input.fadeIn(500);
+  });
+  
+  close_button.on('click',function(){
+    search_button.parent().removeClass('open');
+    close_button.fadeOut(500);
+    input.fadeOut(500);
+  });
+})(jQuery);
